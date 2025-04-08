9f5029efec5016986625c3b2c5ebeb2740730664
Bruno Pinho <20286747+pinhob@users.noreply.github.com>
2025-04-08 21:21:58 +0000

    feat (pokemon.services): get and set cache to pokemon data fetch functions

--- src/services/pokemon.services.ts
+++ src/services/pokemon.services.ts
@@ -1,9 +1,20 @@
-import { AbilityDetail, PokemonData, PokemonResponse, PokemonResult, AllPokemonsResult } from '../types/pokemon.types';
+import { AbilityDetail, PokemonData, PokemonResponse, PokemonResult, AllPokemonsResult } from '../types/pokemon.types.js';
 import { ApiError, AppError, NotFoundError } from '../utils/errors.js';
-import { formatName, capitalize } from '../utils/formatString';
+import { formatName, capitalize } from '../utils/formatString.js';
+import memcacheClient from '../config/memcached.js';
 
 async function getPokemonData(pokemon: string): Promise<PokemonResponse> {
+  const CACHE_KEY = `pokemon_${pokemon}`;
+
   try {
+    const cacheData = await memcacheClient.get<PokemonResponse>(CACHE_KEY);
+
+    if (cacheData && cacheData.value) {
+      console.info('Returning pokemon data from cache');
+      return cacheData.value;
+    }
+
+    console.info('Cache miss, fetching pokemon data from API');
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
     if (!response.ok) {
@@ -14,7 +25,12 @@ async function getPokemonData(pokemon: string): Promise<PokemonResponse> {
       throw new ApiError(`Failed to fetch Pokemon: ${response.statusText}`);
     }
 
-    return response.json();  
+    const data = await response.json();
+
+    await memcacheClient.set(CACHE_KEY, data, { lifetime: 0});
+    console.info(`${pokemon} data saved to cache`);
+
+    return data;  
   } catch (error) {
     if (error instanceof AppError) throw error;
     throw new ApiError('Failed to connect to PokeAPI'); 
@@ -23,7 +39,17 @@ async function getPokemonData(pokemon: string): Promise<PokemonResponse> {
 }
 
 async function getAbilityDetail(url: string): Promise<string> {
+  const CACHE_KEY = `ability_${url}`;
+
   try {
+    const cacheData = await memcacheClient.get<any>(CACHE_KEY);
+
+    if (cacheData && cacheData.value) {
+      console.info('Returning ability data from cache');
+      return cacheData.value;
+    }
+
+    console.info('Cache miss, fetching ability data from API');
     const response = await fetch(url);
 
     if (!response.ok) {
@@ -35,6 +61,9 @@ async function getAbilityDetail(url: string): Promise<string> {
     const englishName = data.names.find(
       (nameEntry: any) => nameEntry.language.name === "en"
     )?.name || data.name;
+
+
+    await memcacheClient.set(CACHE_KEY, englishName, { lifetime: 0});
     
     return englishName; 
   } catch (error) {
@@ -73,7 +102,17 @@ export async function getPokemonAbilities(pokemon: string): Promise<PokemonResul
 }
 
 export async function getAllPokemons(): Promise<AllPokemonsResult[]> {
+    const CACHE_KEY = 'all_pokemons';
+    
     try {
+      const cachedData = await memcacheClient.get<AllPokemonsResult[]>(CACHE_KEY);
+      
+      if (cachedData && cachedData.value) {
+        console.info('Returning pokemons from cache');
+        return cachedData.value;
+      }
+      
+      console.info('Cache miss, fetching all pokemons list from API');
       const data = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1025");
 
       if (!data.ok) {
@@ -82,7 +121,11 @@ export async function getAllPokemons(): Promise<AllPokemonsResult[]> {
 
       const { results } = await data.json();
 
-      const pokemons = results.map((pokemon: any) => (formatName(pokemon.name)))
+      const pokemons = results.map((pokemon: any) => (formatName(pokemon.name)));
+      
+      // Salva no cache sem prazo de validade (0)
+      await memcacheClient.set(CACHE_KEY, pokemons, { lifetime: 0 });
+      console.info('Pokemons list saved to cache');
 
       return pokemons;
     } catch (error) {
